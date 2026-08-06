5a8f00ce3a86ee1d93c88fee97364cf443af2d74
yeonghui704 <yeonghui704@gmail.com>
2026-08-07 01:39:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
 
 YTDL_OPTIONS = {
-    'format': 'bestaudio/best/ba/b',  # 💡 단독 오디오가 없을 경우 비디오+오디오 통합 스트림까지 유연하게 선택하도록 수정
+    'format': 'bestaudio/best/ba/b',  # 💡 단독 오디오가 없을 경우 비디오/오디오 통합 스트림까지 유연하게 선택
     'extractaudio': True,
     'audioformat': 'mp3',
     'outtmpl': 'downloads/%(id)s.%(ext)s',
@@ -112,7 +112,7 @@ YTDL_OPTIONS = {
     'cookiefile': 'cookies.txt',
     'extractor_args': {
         'youtube': {
-            'player_client': ['ios', 'android', 'mweb', 'web']  # 💡 web 클라이언트 항목 추가
+            'player_client': ['mweb', 'ios', 'android', 'web', 'tv']  # 💡 포맷 탐색 클라이언트 다양화
         }
     }
 }
